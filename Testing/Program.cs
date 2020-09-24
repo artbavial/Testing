4d1198e84c31bc01daeb6571f43add91fe3905b3
Vitali Barsukou <pipleworld@gmail.com>
2020-09-24 11:35:47 +0300

    Простой способ подключения к базе данных SQL

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,36 +1,42 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Testing
 {
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                string txt = Console.ReadLine();
+            string connectDataBase = "server=localhost;uid=root;database=testdb;password=########";
 
-                switch (txt)
-                {
-                    case "Привет":
-                        Console.WriteLine("И тебе привет!");
-                        break;
-                    case "Как дела?":
-                        Console.WriteLine("Спасибо, хорошо!");
-                        break;
-                    default:
-                        Console.WriteLine("Такой команды нет.");
-                        break;
-                }
+            MySqlConnection connect = new MySqlConnection(connectDataBase);
+
+            connect.Open();
+
+            for (int x = 1; x <= 7; x++)
+            {
+                string sqlQuery = $"SELECT name FROM tabledb WHERE id = {x}";
+
+                MySqlCommand command = new MySqlCommand(sqlQuery, connect);
+
+                string name = command.ExecuteScalar().ToString();
+
+                Console.WriteLine(name);
             }
 
+            connect.Close();
+
+            Console.ReadKey();
         }
         
     }
